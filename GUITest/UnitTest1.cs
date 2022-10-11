79e89cfe516b68cabda6de28cc688c224f5adb3e
Dmitrii <kondim007@mail.ru>
2022-10-11 20:22:45 +0300

    refactor

--- GUITest/UnitTest1.cs
+++ GUITest/UnitTest1.cs
@@ -1,22 +1,16 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using TestStack.White;
+﻿using TestStack.White;
 using TestStack.White.UIItems;
 using TestStack.White.UIItems.WindowItems;
 using TestStack.White.UIItems.Finders;
-using System.Collections.Generic;
-using TestStack.White.Factory;
-using TestStack.White.UIItems.ListBoxItems;
 using System.Threading;
-using NUnit.Framework;
-using Assert = NUnit.Framework.Assert;
+using Xunit;
 
 namespace GUITest
 {
-    [TestClass]
     public class UnitTest1
     {
-        private readonly static string path = @"C:\Users\Александр Тегин\Desktop\Тестирование\Lab2\Lab2\bin\Debug\Lab2.exe";
+        private const string path = @"C:\Users\kondi\Desktop\TestLab4\Lab2\bin\Debug\Lab2.exe";
+
         public Window AppStart()
         {
             Application application = null;
@@ -27,12 +21,13 @@ namespace GUITest
             return window;
         }
 
-        [TestCase("1", "2", "3")]
-        [TestCase("0,1", "0,2", "0,3")]
-        [TestCase("-1", "-2", "-3")]
-        [TestCase("x", "y", "0")]
-        [TestCase("x", "1", "1")]
-        [TestCase("1", "y", "1")]
+        [Theory]
+        [InlineData("1", "2", "3")]
+        [InlineData("0,1", "0,2", "0,3")]
+        [InlineData("-1", "-2", "-3")]
+        [InlineData("x", "y", "0")]
+        [InlineData("x", "1", "1")]
+        [InlineData("1", "y", "1")]
         public void TestOnPlusClicked(string a, string b, string res)
         {
             var window = AppStart();
@@ -46,17 +41,18 @@ namespace GUITest
             Thread.Sleep(1000);
             sumClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
 
-        [TestCase("1", "2", "-1")]
-        [TestCase("-1", "-2", "1")]
-        [TestCase("0,1", "0,2", "-0,1")]
-        [TestCase("x", "y", "0")]
-        [TestCase("x", "1", "-1")]
-        [TestCase("1", "y", "1")]
+        [Theory]
+        [InlineData("1", "2", "-1")]
+        [InlineData("-1", "-2", "1")]
+        [InlineData("0,1", "0,2", "-0,1")]
+        [InlineData("x", "y", "0")]
+        [InlineData("x", "1", "-1")]
+        [InlineData("1", "y", "1")]
         public void TestOnMinusClicked(string a, string b, string res)
         {
             var window = AppStart();
@@ -70,18 +66,19 @@ namespace GUITest
             Thread.Sleep(1000);
             minusClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
-        
-        [TestCase("1", "2", "2")]
-        [TestCase("-1", "2", "-2")]
-        [TestCase("-1", "-2", "2")]
-        [TestCase("0,1", "0,2", "0,02")]
-        [TestCase("x", "y", "0")]
-        [TestCase("x", "1", "0")]
-        [TestCase("1", "y", "0")]
+
+        [Theory]
+        [InlineData("1", "2", "2")]
+        [InlineData("-1", "2", "-2")]
+        [InlineData("-1", "-2", "2")]
+        [InlineData("0,1", "0,2", "0,02")]
+        [InlineData("x", "y", "0")]
+        [InlineData("x", "1", "0")]
+        [InlineData("1", "y", "0")]
         public void TestOnMultiplyClicked(string a, string b, string res)
         {
             var window = AppStart();
@@ -95,17 +92,18 @@ namespace GUITest
             Thread.Sleep(1000);
             mulClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
 
-        [TestCase("2", "1", "2")]
-        [TestCase("2", "-1", "-2")]
-        [TestCase("0,02", "0,1", "0,2")]
-        [TestCase("x", "y", "Division by zero")]
-        [TestCase("x", "1", "0")]
-        [TestCase("1", "y", "Division by zero")]
+        [Theory]
+        [InlineData("2", "1", "2")]
+        [InlineData("2", "-1", "-2")]
+        [InlineData("0,02", "0,1", "0,2")]
+        [InlineData("x", "y", "Division by zero")]
+        [InlineData("x", "1", "0")]
+        [InlineData("1", "y", "Division by zero")]
         public void TestOnDivideClicked(string a, string b, string res)
         {
             var window = AppStart();
@@ -119,14 +117,15 @@ namespace GUITest
             Thread.Sleep(1000);
             divClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
 
-        [TestCase("1", "", "Empty parameter")]
-        [TestCase("", "1", "Empty parameter")]
-        [TestCase("", "", "Empty parameter")]
+        [Theory]
+        [InlineData("1", "", "Empty parameter")]
+        [InlineData("", "1", "Empty parameter")]
+        [InlineData("", "", "Empty parameter")]
         public void TestOnPlusClickedThrowsException(string a, string b, string res)
         {
             var window = AppStart();
@@ -140,14 +139,15 @@ namespace GUITest
             Thread.Sleep(1000);
             sumClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
 
-        [TestCase("1", "", "Empty parameter")]
-        [TestCase("", "1", "Empty parameter")]
-        [TestCase("", "", "Empty parameter")]
+        [Theory]
+        [InlineData("1", "", "Empty parameter")]
+        [InlineData("", "1", "Empty parameter")]
+        [InlineData("", "", "Empty parameter")]
         public void TestOnMinusClickedThrowsException(string a, string b, string res)
         {
             var window = AppStart();
@@ -161,18 +161,19 @@ namespace GUITest
             Thread.Sleep(1000);
             minusClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
 
-        [TestCase("1", "", "Empty parameter")]
-        [TestCase("", "1", "Empty parameter")]
-        [TestCase("", "", "Empty parameter")]
+        [Theory]
+        [InlineData("1", "", "Empty parameter")]
+        [InlineData("", "1", "Empty parameter")]
+        [InlineData("", "", "Empty parameter")]
         public void TestOnMultiplyClickedThrowsException(string a, string b, string res)
         {
             var window = AppStart();
-            TextBox firstArg = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbA"));
+            var firstArg = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbA"));
             Thread.Sleep(1000);
             firstArg.SetValue(a);
             TextBox secArg = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbB"));
@@ -182,19 +183,20 @@ namespace GUITest
             Thread.Sleep(1000);
             mulClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
 
-        [TestCase("1", "0", "Division by zero")]
-        [TestCase("1", "0,00000001", "Division by zero")]
-        [TestCase("1", "0,000000009", "Division by zero")]
-        [TestCase("1", "-0,00000001", "Division by zero")]
-        [TestCase("1", "-0,000000009", "Division by zero")]
-        [TestCase("1", "", "Empty parameter")]
-        [TestCase("", "1", "Empty parameter")]
-        [TestCase("", "", "Empty parameter")]
+        [Theory]
+        [InlineData("1", "0", "Division by zero")]
+        [InlineData("1", "0,00000001", "Division by zero")]
+        [InlineData("1", "0,000000009", "Division by zero")]
+        [InlineData("1", "-0,00000001", "Division by zero")]
+        [InlineData("1", "-0,000000009", "Division by zero")]
+        [InlineData("1", "", "Empty parameter")]
+        [InlineData("", "1", "Empty parameter")]
+        [InlineData("", "", "Empty parameter")]
         public void TestOnDivideClickedThrowsException(string a, string b, string res)
         {
             var window = AppStart();
@@ -208,13 +210,14 @@ namespace GUITest
             Thread.Sleep(1000);
             divClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
 
-        [TestCase("1", "0,000000011", "Division by zero")]
-        [TestCase("1", "-0,000000011", "Division by zero")]
+        [Theory]
+        [InlineData("1", "0,000000011", "Division by zero")]
+        [InlineData("1", "-0,000000011", "Division by zero")]
         public void TestOnDivideClickedNotThrowsException(string a, string b, string res)
         {
             var window = AppStart();
@@ -228,7 +231,7 @@ namespace GUITest
             Thread.Sleep(1000);
             divClick.Click();
             TextBox Display = window.Get<TextBox>(SearchCriteria.ByAutomationId("tbMessage"));
-            Assert.That(res, Is.Not.EqualTo(Display.Name.ToString()));
+            Assert.Equal(res, Display.Name);
             Thread.Sleep(2000);
             window.Close();
         }
